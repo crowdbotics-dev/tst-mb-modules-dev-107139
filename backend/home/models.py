42395588bbbccdf8dc7d08d8dfefccde315cc1dc
Saurav Gharti Magar <sp.gharti@gmail.com>
2023-09-12 12:12:49 +0545

    Update models.py

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+import Hey
+
 class Hello(models.Model):
     'Generated Model'
     image = models.ImageField(upload_to="hello/image/images/",)
